3d3a706766275681e222a2c2e9d4a5d789b68626
morarudia97 <diana.moraru@FEEL11-LP.feelit.iasi.net>
2019-09-09 13:22:42 +0300

    update todo

--- todo_app/todo.js
+++ todo_app/todo.js
@@ -1,8 +1,9 @@
 function renderList() {  
+	document.getElementsByClassName('list')[0].innerHTML="";
 	const carteLista = JSON.parse(localStorage.getItem('book-info'));
 	if(carteLista) {
 		carteLista.map((element, index) => {
-			let container = '<div class="row"><div>'+element.denumireCarte+'</div><div>'+element.autor+'</div><div>'+element.anPublicatie+'</div><button class="btn-update" data-id="'+index+'">Update</btn><button class="btn-delete" data-id="'+index+'">Delete</button></div>';
+			let container = '<div class="row"><div>'+element.denumireCarte+'</div><div>'+element.updateDenumire+'</div><div>'+element.autor+'</div><div>'+element.updateAutor+'</div><div>'+element.anPublicatie+'</div><div>'+element.updateAn+'</div><div class="buttonDeUp" ><button class="btn-update" data-id="'+index+'">Update</btn><button class="btn-delete" data-id="'+index+'">Delete</button></div></div>';
 			document.getElementsByClassName('list')[0].innerHTML += container;
 		});
 	}
@@ -12,34 +13,39 @@ function renderList() {
 function addlistener() {
 	const updateBtns = document.getElementsByClassName('btn-update');
 	for(let i = 0; i < updateBtns.length; i++) {
-		updateBtns[i].addEventListener('click', function(e) {
-			console.log(this.dataset.id);
+		updateBtns[i].addEventListener('click', function(e){
 			const id =  parseInt(this.dataset.id);
 			const carteLista = JSON.parse(localStorage.getItem('book-info'));
-			carteLista[id].updateAn=document.getElementById('update-an').value;
-			carteLista[id].updateAutor = document.getElementById('update-autor').value;
-			carteLista[id].updateDenumire = document.getElementById('update-name').value;
-			localStorage.setItem('book-info', JSON.stringify(carteLista));
-		});
-	}
-	// const deleteBtns = document.getElementsByClassName('btn-delete');
-	// for(let i = 0; i < deleteBtns.length; i++) {
-	// 	deleteBtns.addEventListener('click',() =>{
-	// 	const id =  parseInt(this.dataset.id);
-	// 	const carteLista = JSON.parse(localStorage.getItem('book-info'));
-	// 	if(
-	// 		carteLista === null) {
-	// 		carteLista = [];
-	// 		carteLista.splice(id,1);
-	// 	}
-	// 	localStorage.setItem('book-info', JSON.stringify(carteLista));
-	// });
-// 	let deleteit = carteLista.map((element,index) =>{
-// carteLista.splice(id,1)
-// 	})
+			// const objectUpdate ={
+			// carteLista[id].updateAn=document.getElementById('add-an').value;
+			// carteLista[id].updateAutor = document.getElementById('add-autor').value;
+			// carteLista[id].updateDenumire = document.getElementById('add-name').value;}
+				const objectUpdate = {
+		"updateAn": document.getElementById('add-an').value,
+		"updateAutor": document.getElementById('add-autor').value,
+		"updateDenumire": document.getElementById('add-name').value
+	};
+		if(carteLista !== null) {
+		carteLista.push(objectUpdate);
+	} });
+	localStorage.setItem('book-info', JSON.stringify(carteLista));
+
+	};
+	
+	const deleteBtns = document.getElementsByClassName('btn-delete');
+	for(let i = 0; i < deleteBtns.length; i++) {
+		deleteBtns[i].addEventListener('click',function(e){
+		const id =  parseInt(this.dataset.id);
+		const carteLista = JSON.parse(localStorage.getItem('book-info'));
+		if(carteLista !== null) {
+			carteLista.splice(id,1);
+			}
+		localStorage.setItem('book-info', JSON.stringify(carteLista));
+		renderList();
+		
+		})
+	};
 }
-
-
 renderList();
 
 document.getElementById('add-book').addEventListener('click', () => {
@@ -49,8 +55,6 @@ document.getElementById('add-book').addEventListener('click', () => {
 document.getElementById('cancel-add').addEventListener('click', () => {
 	document.getElementsByClassName('add-pop-up')[0].classList.add('displayNone');
 });
-
-
 
 document.getElementById('save-add').addEventListener('click', () => {
 	if(!validateform()) {
